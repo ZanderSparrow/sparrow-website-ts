a12ed5de22ec69260807c8da5e7efde28c094255
Zander Sparrow <alexsilverhawk@gmail.com>
2024-01-18 12:04:42 -0800

    Remove react-router-dom

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,21 @@
 import * as React from "react"
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import type { HeadFC, PageProps } from "gatsby"
 import './index.css'
 
 import App from "./App"
 
+// const IndexPage: React.FC = () => (
+// 	<BrowserRouter>
+// 		<Routes>
+// 			<Route path="/" element={<App />} />
+// 		</Routes>
+// 	</BrowserRouter>
+// );
+
 const IndexPage = ({}: PageProps) => {
   return (
     <main>
-      <BrowserRouter>
- 		    <Routes>
-			    <Route path="/" element={<App />} />
-		    </Routes>
- 	    </BrowserRouter>
+      <App />
     </main>
   )
 }
