6de1c50bd3345cb0f8191d2468d068febca82f8d
Zander Sparrow <alexsilverhawk@gmail.com>
2024-01-22 12:41:03 -0800

    Fix typo in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,10 +8,11 @@ const Footer = () => {
     return (
         <div id="sticky-footer">
             <div className="wrap">
-            <p>
-                <SocialIcon className="social" network="linkedin" url="https://www.linkedin.com/in/zandersparrow/"/>
-                <SocialIcon className="social" network="github" url="https://github.com/ZanderSparrow"/>
-                Parallax base code based on <a href="https://codesandbox.io/p/sandbox/vertical-parallax-h1rrv">Vertical Parallax Example</a></p>
+                <p>
+                    <SocialIcon className="social" network="linkedin" url="https://www.linkedin.com/in/zandersparrow/"/>
+                    <SocialIcon className="social" network="github" url="https://github.com/ZanderSparrow"/>
+                    Parallax based on <a href="https://codesandbox.io/p/sandbox/vertical-parallax-h1rrv">Vertical Parallax Example</a>
+                </p>
             </div>
         </div>
     )
