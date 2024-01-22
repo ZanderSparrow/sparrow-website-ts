15cedc67a857a5bf5fba88122902381add624c94
Zander Sparrow <alexsilverhawk@gmail.com>
2024-01-22 12:38:38 -0800

    Add social media links to github and linkedin

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { SocialIcon } from 'react-social-icons'
+import 'react-social-icons/github'
+import 'react-social-icons/linkedin'
 import './footer.css';
 
 const Footer = () => {
     return (
         <div id="sticky-footer">
             <div className="wrap">
-                <p>Parallax base code based on <a href="https://codesandbox.io/p/sandbox/vertical-parallax-h1rrv">Vertical Parallax Example</a></p>
+            <p>
+                <SocialIcon className="social" network="linkedin" url="https://www.linkedin.com/in/zandersparrow/"/>
+                <SocialIcon className="social" network="github" url="https://github.com/ZanderSparrow"/>
+                Parallax base code based on <a href="https://codesandbox.io/p/sandbox/vertical-parallax-h1rrv">Vertical Parallax Example</a></p>
             </div>
         </div>
     )
